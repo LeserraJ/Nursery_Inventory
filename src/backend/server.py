654bcbdcee754440421d4d4ef2e3292063307aac
John Leserra <leserrabusiness@gmail.com>
2023-09-17 22:43:32 -0400

    connected server to database

--- src/backend/server.py
+++ src/backend/server.py
@@ -1,11 +1,20 @@
-from Flask import Flask, request, jsonify
+from flask import Flask, request, jsonify
+import products_dao
+from sql_connection import get_sql_connection
 
 app = Flask(__name__)
 
-@app.route('/')
 
-def hello():
-    return "Hello, how are you?"
+connection = get_sql_connection()
+
+
+@app.route('/getProducts', methods=['GET'])
+
+def get_products():
+    products = products_dao.get_all_products(connection)
+    response = jsonify(products)
+    response.headers.add('Access-Control-Allow-Origin','*')
+    return response
 
 if __name__ == "__main__" :
     print("Starting Python Flask server For Plant Nursery Managment System")
